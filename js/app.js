e492681c68f0d63875ea6f0a0eb1c71b0c4f216f
khaledfarghly <khaledfarghly9991@gmail.com>
2021-08-23 05:18:07 +0200

    add active class to the section when near top of viewport through scroll

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@
  */
 const mainContent = document.querySelector("main");
 const sections = document.querySelectorAll("section");
-const navbarMenu = document.querySelector(".navbar-menu");
+const navbarMenu = document.querySelector(".navbar__menu");
 const navbarList = document.querySelector("#navbar__list");
 // let newSection = oneSection.setAttribute("id", "section4");
 let newli;
@@ -38,34 +38,61 @@ let newli;
  *
  */
 
-// build the nav list
-// add li element with thier section name to navbar
+// build the nav list item
+
+// create li element for nav
 sections.forEach((elem, indx, arr) => {
-  // create new li element
   newLi = document.createElement("li");
-  navbarList.appendChild(newLi);
 
-  // Get the name of the specific section to set in li element
+  // Get the data-nav of the specific section to set inner li element
   let dataNav = elem.getAttribute("data-nav");
-  newLi.textContent = dataNav;
+  newLi.innerHTML = dataNav;
 
-  console.log(dataNav);
-});
-
-// Scroll to anchor ID using scrollTO event
-sections.forEach((elem, indx, arr) => {
-  // listening for li item and scoll to it
+  // Set the class attr to styling list item
+  newLi.setAttribute("class", "menu__link");
+  // listening for click on li element and scroll to it
   newLi.addEventListener("click", () => {
     elem.scrollIntoView({ behavior: "smooth", block: "end" });
   });
+
+  //   console.log(dataNav);
+  navbarList.appendChild(newLi);
 });
+
 // Add class 'active' to section when near top of viewport
+const changeActiveSec = () => {
+  for (section of sections) {
+    // get the top of viewport
+    let sectionView = section.getBoundingClientRect();
+    let topOfSectionView = sectionView.top;
+
+    // console.log(topOfSectionView);
+    // // console.log(sectionView);
+
+    // check which section in viewport closer to top
+    if (
+      // this number might need changed by follow viewport needs
+      topOfSectionView >= -50 &&
+      topOfSectionView <= 400 &&
+      !section.classList.contains("your-active-class")
+    ) {
+      // if sec viewport true and not has active class
+      section.classList.add("your-active-class");
+    } else {
+      // if false remove it
+      section.classList.remove("your-active-class");
+    }
+  }
+};
+document.addEventListener("scroll", changeActiveSec);
 
 /**
  * End Main Functions
  * Begin Events
  *
  */
+
+// Scroll to anchor ID using scrollTO event
 
 // Build menu
 
