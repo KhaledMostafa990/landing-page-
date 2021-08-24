c505799a886e9fece8ae84ed0b0061f8be1c0544
khaledfarghly <khaledfarghly9991@gmail.com>
2021-08-24 04:37:18 +0200

    finsh the requirement!

--- README.md
+++ README.md
@@ -7,4 +7,45 @@ The project has starter code some HTML and CSS styling to display a static versi
 I'll need to convert this project from a static project to an interactive one.
 This will require modifying the HTML and CSS files, but by JavaScript file.
 
-landing page project
+landing page project are the first exam in Web Dev professional track nanodegree
+by Udacity, eg FWD, itida.
+
+the project are usful to practice on what we learn before through the nanodegree.
+the landing also requier from us research to get some methods we are not learning yet.
+
+##### requirments
+
+## 1- add new section to html (not require javascript)
+
+at first I declared a variables which I needs, and by the time I will add any helper function as I need
+
+## 2- add list item and append it to unorder list on html
+
+I build the nav list items and add it to navgation then styling it.
+by use forEach method to loop over the sections and creates the items by use "data-nav" attrbuite in sections.
+
+## -3 when click to any list item on navgation it should scroll to thier section (without "href${id}")
+
+and through the loop over sections I used "addEvenListener" and "scrollInTo" methods to finsh it.
+
+## 4- if any section in viewport through scrolling or not it should have class active-state.
+
+by using for loop with getBoundingClientRect(), to defind which section in viewport through iterations, to add active class to every section near to top of viewport, and use event listener to do that through scrolling.
+
+###### addtions (not required)
+
+## 5- Add an active state to navigation items when a section is in the viewport.
+
+set background color in css to class to make it as active state .
+by using nested forEach loop on sections to get viewport and loop through the list items with viewport data on the section and use conditions to add/remove the active state to item in viewport.
+
+## -6 build a button on fixed postions for scroll to the top of docuemnt
+
+simply I created div and span , and style it's by ".cssText" property,
+next to "scrollTo" method, to scroll in the top of document ,and hide it if you in top of document.
+
+## -7 Hide fixed navigation bar while scrolling (it should still be present on page load).
+
+by using event listener on scrolling I create a hidden class and toggle on it.
+
+# final project link
--- js/app.js
+++ js/app.js
@@ -94,7 +94,11 @@ display:inline-block;
   });
 });
 
-// Add class 'active' to section when near top of viewport
+/**
+ *
+ * Add class 'active' to section when near top of viewport
+ *
+ **/
 const changeActiveSec = () => {
   for (section of sections) {
     // get the top of viewport
@@ -117,63 +121,7 @@ document.addEventListener("scroll", changeActiveSec);
 
 /**
  *
- * Hide navbar on scroll
- *
- **/
-
-const hideNav = () => {
-  if (!navbarList.classList.contains("hidden")) {
-    // set timeout between add and remove the hidden class
-    setTimeout(() => {
-      addClass(navbarList, "hidden");
-    }, 200);
-  } else {
-    setTimeout(() => {
-      removeClass(navbarList, "hidden");
-    }, 1000);
-  }
-  // return document.addEventListener("scroll", hideNav);
-};
-// hideNav();
-document.addEventListener("scroll", hideNav);
-
-/**
- *
- * create button to scroll to top
- *
- **/
-
-const scrollToTopBtn = document.createElement("div");
-
-scrollToTopBtn.classList.add("btn"); // styling Div btn In Helper function
-
-// add div to main content
-mainContent.insertAdjacentElement("afterend", scrollToTopBtn);
-
-// add the style btn function if view not in the top.. else remove it.
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= 600) {
-    addBtnStyle(scrollToTopBtn);
-  } else {
-    removeBtnStyle(scrollToTopBtn);
-  }
-});
-
-// create elemnt for text btn
-const scrollText = document.createElement("span");
-scrollText.textContent = "Top";
-// add text to div with opposite rotate style
-scrollToTopBtn.appendChild(scrollText);
-scrollText.style = `transform: rotate(-45deg);`;
-
-//  listener for click event to scroll to top
-scrollToTopBtn.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
-
-/**
- *
- * show which section in the nav is viewed while scroling
+ * add active state to item of section in viewport
  *
  */
 // get an li list as array
@@ -208,7 +156,54 @@ const activeItemOfsec = () => {
 
 document.addEventListener("scroll", activeItemOfsec);
 
-// console.log(sections);
+/**
+ * create button to scroll to top
+ **/
+
+const scrollToTopBtn = document.createElement("div");
+scrollToTopBtn.classList.add("btn"); // styling Div btn In Helper function
+// add div to main content
+mainContent.insertAdjacentElement("afterend", scrollToTopBtn);
+
+// add the style btn function if view not in the top.. else remove it.
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 600) {
+    addBtnStyle(scrollToTopBtn);
+  } else {
+    removeBtnStyle(scrollToTopBtn);
+  }
+});
+
+// create elemnt for text btn
+const scrollText = document.createElement("span");
+scrollText.textContent = "Top";
+// add text to div with opposite rotate style
+scrollToTopBtn.appendChild(scrollText);
+scrollText.style = `transform: rotate(-45deg);`;
+
+//  listener for click event to scroll to top
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+/**
+ *
+ * Hide navbar on scroll
+ *
+ **/
+const hideNav = () => {
+  if (!navbarList.classList.contains("hidden")) {
+    // set timeout between add and remove the hidden class
+    setTimeout(() => {
+      addClass(navbarList, "hidden");
+    }, 200);
+  } else {
+    setTimeout(() => {
+      removeClass(navbarList, "hidden");
+    }, 600);
+  }
+};
+document.addEventListener("scroll", hideNav);
 /**
  * End Main Functions
  */
